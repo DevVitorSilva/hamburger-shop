a0ee2609e3aa4156af185de1f03efde534f6cc76
DevVitorSilva <joao@joaovitor.com>
2023-08-21 09:12:18 -0300

    initial setting.

---
+++ README.md
@@ -0,0 +1,6 @@
+problema: construir uma hamburger-shop, com as funcionalidades de:
+1 Fazer pedido
+2 Ver todos os pedidos
+2.1 Marcar o pedido como: pedido feito, em preparo, concluido
+2.2 Listar pedidos como: feitos, em preparo e concluido
+3 Poder adicionar um novo produto ao cardápio
--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import e from 'express';
+import {config} from 'dotenv'
+
+config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 9001
+const app = e();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
