4e762b3122175da262dc05647c032f07bb90c9ef
DevVitorSilva <joao@joaovitor.com>
2023-08-23 17:29:57 -0300

    correcting error in route /, and orders route created.

--- routes/orders.js
+++ routes/orders.js
@@ -1,24 +1,53 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import { BreadsModel } from "../models/Breads.js";
 import { MeatModel } from "../models/Meat.js";
 import { OptionalModel } from "../models/Optional.js";
 import { StatusModel } from "../models/Status.js";
+import { OrdersModel } from "../models/Orders.js";
 
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const bread = await BreadsModel.find({})
-    const meat = await MeatModel.find({})
-    const optional = await OptionalModel.find({})
+    try {
+        const bread = await BreadsModel.find({})
+        const meat = await MeatModel.find({})
+        const optionals = await OptionalModel.find({})
 
-    res.render('index', {bread, meat, optional})
+        res.status(200).render('index', { bread, meat, optionals })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(`
+        <div>
+            <h1>500 - Internal server error</h1>
+            <h2>Oops! Ocorreu um erro!</h2>
+            <h2>Fique tranquilo, estamos cientes e trabalhando na correção.</h2>
+        </div>
+        `)
+    }
 })
 
 router.get('/request', (req, res) => {
     res.render('request')
 })
 
-// criar a rota com metodo http POST e verificar se o nome do cliente pode vir com espaço
+router.post('/orders', e.urlencoded({ extended: true }), async (req, res) => {
+
+    const { name, bread, meat, optionals } = req.body
+
+    if (!name) {
+        return res.status(412).send('Name não pode ser vazio.')
+    } else if (!bread) {
+        return res.status(412).send('Bread não pode ser vazio.')
+    } else if (!meat) {
+        return res.status(412).send('Meat não pode ser vazio.')
+    }
+
+    const orders = new OrdersModel({ name, bread, meat, optionals })
+
+    await orders.save()
+
+    res.status(201).redirect('/')
+})
 
 export {
     router
